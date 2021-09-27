8b58f1b747b244aac1792c93a4587fa74ff84e0e
thomazjorge.zetta@ufla.br <thomazjorge.zetta@ufla.br>
2021-09-27 09:32:40 -0300

    Adicionando mensagem do imc

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -18,8 +18,22 @@ export class HomePage {
   }
 
   onCalculate() {
+
     const imc = this.weight / (this.height * this.height);
-    this.showMessage(`IMC = ${imc.toFixed(2)}`);
+
+    let mensagem = '';
+    if(imc < 18.5) {
+      mensagem = 'MAGREZA';
+    } else if (imc <= 24.9) {
+      mensagem = 'NORMAL';
+    } else if (imc <= 29.9) {
+      mensagem = 'SOBREPESO';
+    } else if (imc <= 39.9) {
+      mensagem = 'OBESIDADE';
+    } else {
+      mensagem = 'OBESIDADE GRAVE';
+    }
+    this.showMessage(`IMC = ${imc.toFixed(2)}, e você está classificado em ${mensagem}!`);
   }
 
   async showMessage(msg: string) {
